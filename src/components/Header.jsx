81e85d10be8a4b7dffd5e810d29fb3ca78e1fb63
monuixd <monu.webdesigner@gmail.com>
2022-02-23 17:21:55 +0530

    bootstrap

---
+++ src/components/Header.jsx
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import {
+    Navbar,
+    NavbarBrand,
+    NavbarToggler,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+    NavbarText
+} from 'reactstrap';
+
+class Header extends Component {
+
+    render() {
+
+        return (
+            <Navbar
+                color="warning"
+                expand="md"
+                fixed="top"
+                light
+            >
+                <NavbarBrand href="/">
+                    Reactstrap Pro+
+                </NavbarBrand>
+                <NavbarToggler onClick={function noRefCheck() { }} />
+                <Collapse navbar>
+                    <Nav
+                        className="me-auto"
+                        navbar
+                    >
+                        <NavItem>
+                            <NavLink href="/components/">
+                                Components
+                            </NavLink>
+                        </NavItem>
+                        <NavItem>
+                            <NavLink href="https://github.com/reactstrap/reactstrap">
+                                GitHub
+                            </NavLink>
+                        </NavItem>
+                        <UncontrolledDropdown
+                            inNavbar
+                            nav
+                        >
+                            <DropdownToggle
+                                caret
+                                nav
+                            >
+                                Options
+                            </DropdownToggle>
+                            <DropdownMenu right>
+                                <DropdownItem>
+                                    Option 1
+                                </DropdownItem>
+                                <DropdownItem>
+                                    Option 2
+                                </DropdownItem>
+                                <DropdownItem divider />
+                                <DropdownItem>
+                                    Reset
+                                </DropdownItem>
+                            </DropdownMenu>
+                        </UncontrolledDropdown>
+                    </Nav>
+                    <NavbarText>
+                        Simple Text
+                    </NavbarText>
+                </Collapse>
+            </Navbar>
+        )
+
+    }
+
+}
+
+export default Header;
