933fc28a9602e2a01ec643e2bc1ac130f4ff27af
MONUiXD <42413791+monuixd@users.noreply.github.com>
2022-03-07 17:56:55 +0530

    props

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import Header from './components/Header';
+import Home from './components/home';
+
+function App() {
+  return (
+    <>
+
+      <Header />
+      <Home />
+
+    </>
+
+  );
+}
+
+export default App;
